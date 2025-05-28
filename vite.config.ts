fa6d97ab27fc6248b7caf6b17874bf86b21d1e19
enoreyes <enoreyes@gmail.com>
2025-05-27 17:56:46 -0700

    fix: add tailwind + shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+import path from "path"
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
   // Vite server options
   server: {
     // Tauri expects a fixed port, not a random one
